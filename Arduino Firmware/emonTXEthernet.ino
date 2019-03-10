154f2aae4c50d1cd8b117dda9781bab56060d65c
Wouter J <WouterJansen@users.noreply.github.com>
2019-03-10 16:53:21 +0100

    Update emonTXEthernet.ino

--- Arduino Firmware/emonTXEthernet.ino
+++ Arduino Firmware/emonTXEthernet.ino
@@ -43,11 +43,14 @@ void setup()
     // try to congifure using IP address instead of DHCP:
     Ethernet.begin(mac, ip);
   }
+  Ethernet.setRetransmissionTimeout(300);
+  Ethernet.setRetransmissionCount(1); 
   // Ethernet warmup delay
   delay(2000);
   Serial.println("EMONTX HTTP client started!"); 
   // First argument is analog pin used by CT connection, second is calibration factor. 
   // Calibration factor = CT ratio / burden resistance = (100A / 0.05A) / 33 Ohms = 60.606
+  // First argument is analog pin used by CT connection
   ct1.current(1, 60.606);
   ct2.current(2, 60.606);
   ct3.current(3, 60.606);
@@ -63,7 +66,6 @@ void setup()
   ct4.voltage(0, 245, 1.7);
   // Setup indicator LED
   pinMode(LEDpin, OUTPUT);
-  digitalWrite(LEDpin, HIGH);
 }
 
 void loop()
@@ -73,24 +75,29 @@ void loop()
   ct2.calcVI(20, 2000);
   ct3.calcVI(20, 2000);
   ct4.calcVI(20, 2000);
-  // Extract individual elements (realpower,Vrms and Irms)
-  // and use them as arguments for printing and sending the data. First argument is node name.
-  printData("ct1", ct1.realPower, ct1.Vrms, ct1.Irms);
-  sendData("ct1", ct1.realPower, ct1.Vrms, ct1.Irms);
-  printData("ct2", ct2.realPower, ct2.Vrms, ct2.Irms);
-  sendData("ct2", ct2.realPower, ct2.Vrms, ct2.Irms);
-  printData("ct3", ct3.realPower, ct3.Vrms, ct3.Irms);
-  sendData("ct3", ct3.realPower, ct3.Vrms, ct3.Irms);
-  printData("ct4", ct4.realPower, ct4.Vrms, ct4.Irms);
-  sendData("ct4", ct4.realPower, ct4.Vrms, ct4.Irms);
-  //Small interval delay to limit the data stream
-  delay(postingInterval);
+  // If the posting interval has passed since your last connection,
+  // then connect again and send data:
+  if (millis() - lastConnectionTime > postingInterval) {
+    digitalWrite(LEDpin, HIGH);
+    // Extract individual elements (realpower,Vrms and Irms)
+    // and use them as arguments for printing and sending the data. First argument is node name.
+    printData("ct1", ct1.realPower, ct1.Vrms, ct1.Irms);
+    sendData("ct1", ct1.realPower, ct1.Vrms, ct1.Irms);
+    printData("ct2", ct2.realPower, ct2.Vrms, ct2.Irms);
+    sendData("ct2", ct2.realPower, ct2.Vrms, ct2.Irms);
+    printData("ct3", ct3.realPower, ct3.Vrms, ct3.Irms);
+    sendData("ct3", ct3.realPower, ct3.Vrms, ct3.Irms);
+    printData("ct4", ct4.realPower, ct4.Vrms, ct4.Irms);
+    sendData("ct4", ct4.realPower, ct4.Vrms, ct4.Irms);
+    digitalWrite(LEDpin, LOW);
+  }
 }
+
 // this method makes a HTTP connection to the EmonCMS server and sends the data in correct format
 void sendData(String node, float realPower, float supplyVoltage, float Irms) {
   // if there's a successful connection:
   if (client.connect(emoncmsserver, 80)) {
-    Serial.print("Connecting and sending JSON packet for node ");
+    Serial.print("Connecting and sending JSON packet for Node ");
     Serial.println(node);
     // send the HTTP PUT request:
     client.print("GET /emoncms/input/post?node=");
@@ -110,12 +117,14 @@ void sendData(String node, float realPower, float supplyVoltage, float Irms) {
     client.println("Connection: close");
     client.println();
     client.stop();
+    lastConnectionTime = millis();
   }
   else {
     Serial.println("Could not connect to server. Disconnecting!");
     client.stop();
   }
 }
+
 // Serial print out information on the node for debugging
 void printData(String node, float realPower, float supplyVoltage, float Irms) {
   Serial.print("Measurement taken for Node ");
